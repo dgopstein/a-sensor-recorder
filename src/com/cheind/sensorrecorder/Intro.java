f713123f52b8d5c5d78a52557b0862a3a0fb3189
christoph.heindl <christoph.heindl@39f818c9-4827-a5d0-f524-e791c2b9bfd2>
2010-04-28 19:04:28 +0000

    initial version displays a list of sensors along with description and a button to request recording.

--- src/com/cheind/sensorrecorder/Intro.java
+++ src/com/cheind/sensorrecorder/Intro.java
@@ -1,13 +1,20 @@
 package com.cheind.sensorrecorder;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Intro extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.intro);
+        
+        ListView sensor_list = (ListView) findViewById(R.id.sensor_list);
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor_list.setAdapter(new SensorListAdapter(this, sm.getSensorList(Sensor.TYPE_ALL)));
     }
 }
