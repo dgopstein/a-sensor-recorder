e11e1acea5d450c63139a08ddd2dbf8424ae29ce
christoph.heindl <christoph.heindl@39f818c9-4827-a5d0-f524-e791c2b9bfd2>
2010-04-30 18:21:31 +0000

    intro page is working: the list of sensors is shown along with some dummy text and a check box signaling the selected state of each item.

--- src/com/cheind/sensorrecorder/Intro.java
+++ src/com/cheind/sensorrecorder/Intro.java
@@ -4,13 +4,7 @@ import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
-
-import com.cheind.sensorrecorder.SensorListAdapter.ViewHolder;
 
 public class Intro extends Activity {
     /** Called when the activity is first created. */
@@ -20,25 +14,9 @@ public class Intro extends Activity {
         setContentView(R.layout.intro);
         
         ListView sensor_list = (ListView) findViewById(R.id.sensor_list);
-        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor_list.setItemsCanFocus(false);
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);       
+        
         sensor_list.setAdapter(new SensorListAdapter(this, sm.getSensorList(Sensor.TYPE_ALL)));
-        sensor_list.setOnItemClickListener(new OnItemClickListener() {
-
-          @Override
-          public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
-            ListView lv = (ListView) arg0;
-            SensorListAdapter.ViewHolder vh = (ViewHolder) arg1.getTag();
-            boolean check = !vh.check.isChecked();
-            vh.check.setChecked(check);
-            lv.setItemChecked(position, check);
-            
-            String s ="";
-            for(long cid : lv.getCheckItemIds()) {
-              s += cid +  ",";
-            }
-            Log.i("juhu", s);
-            
-          }
-        });
     }
 }
