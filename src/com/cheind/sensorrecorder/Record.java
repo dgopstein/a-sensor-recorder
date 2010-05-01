38976e151c4f308dedba917c5d09800a7ecc0e77
christoph.heindl <christoph.heindl@39f818c9-4827-a5d0-f524-e791c2b9bfd2>
2010-05-01 13:35:07 +0000

    added initial Record activity to display live values from sensors. fixed alert to be displayed when no sensor is checked.

---
+++ src/com/cheind/sensorrecorder/Record.java
@@ -0,0 +1,70 @@
+package com.cheind.sensorrecorder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class Record extends Activity {
+  
+  SensorManager _sm;
+  List<Sensor> _sensors;
+  List<GenericSensorEventListener> _listeners;
+  LayoutInflater _inflater;
+  TableLayout _table;
+  
+  public void onCreate(Bundle savedInstance) {
+    super.onCreate(savedInstance);
+    _sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+    _sensors = _sm.getSensorList(Sensor.TYPE_ALL);
+    _inflater = this.getLayoutInflater();
+    _listeners = new ArrayList<GenericSensorEventListener>();
+    
+    this.setContentView(R.layout.record);
+    _table = (TableLayout) findViewById(R.id.table_of_values);
+  }
+  
+  @Override
+  public void onResume() {
+    super.onResume();
+    
+    _table.removeAllViews();
+    _listeners.clear();
+    
+    // Intent sent by Intro containing the selected
+    // sensors as a list of ids
+    Intent intent = this.getIntent();
+    ArrayList<Integer> sensor_ids = intent.getIntegerArrayListExtra("selected.sensors");
+    for(int si : sensor_ids) {
+      Sensor s = _sensors.get(si);
+      TableRow row = (TableRow)_inflater.inflate(R.layout.sensor_row, _table, false);
+      TextView sensor_name = (TextView)row.findViewById(R.id.sensor_column_name);
+      TextView sensor_values = (TextView)row.findViewById(R.id.sensor_column_values);
+      
+      sensor_name.setText(s.getName());
+      sensor_values.setText("no values yet");
+      
+      GenericSensorEventListener gsel = new GenericSensorEventListener(sensor_values);
+      _sm.registerListener(gsel, s, SensorManager.SENSOR_DELAY_UI);
+      _table.addView(row);
+      _listeners.add(gsel);
+    }
+  }
+  
+  @Override
+  public void onStop() {
+    super.onStop();
+    for(GenericSensorEventListener l : _listeners) {
+      _sm.unregisterListener(l);
+    }
+  }
+
+}
