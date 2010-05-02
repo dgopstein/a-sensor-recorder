10499a999b48e7dcc6bf6926b13c5db79b55535a
christoph.heindl <christoph.heindl@39f818c9-4827-a5d0-f524-e791c2b9bfd2>
2010-05-02 09:03:10 +0000

    Added base class for sensor event listener to distinguish better between visual and file listeners.

---
+++ trunk/src/com/cheind/sensorrecorder/UISensorEventListener.java
@@ -0,0 +1,23 @@
+package com.cheind.sensorrecorder;
+
+import java.text.DecimalFormat;
+
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+public class UISensorEventListener extends GenericSensorEventListener {
+  
+  static final String VISUAL_DELIMITER = ", ";
+  
+  private TextView _tv;
+  
+  public UISensorEventListener(TextView tv) {
+    super(new DecimalFormat("0.00"), VISUAL_DELIMITER);
+    _tv = tv;
+  }
+  
+  @Override
+  public void onSensorChanged(SensorEvent ev) {
+    _tv.setText(this.formatValues(ev.values));
+  }
+}
